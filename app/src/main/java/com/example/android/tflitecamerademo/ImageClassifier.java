f5db0e7ece6b4b1ed502ce0e287b281ed718f995
gerryagustian97 <31478259+gerryagustian97@users.noreply.github.com>
2019-07-10 15:07:15 +0700

    Update model

--- app/src/main/java/com/example/android/tflitecamerademo/ImageClassifier.java
+++ app/src/main/java/com/example/android/tflitecamerademo/ImageClassifier.java
@@ -45,10 +45,10 @@ public class ImageClassifier {
   private static final String TAG = "TfLiteCameraDemo";
 
   /** Name of the model file stored in Assets. */
-  private static final String MODEL_PATH = "graph.lite";
+  private static final String MODEL_PATH = "model_uang_10_07.lite";
 
   /** Name of the label file stored in Assets. */
-  private static final String LABEL_PATH = "labels.txt";
+  private static final String LABEL_PATH = "model_uang_10_07_labels.txt";
 
   /** Number of results to show in the UI. */
   private static final int RESULTS_TO_SHOW = 2;
